8a97e93d97ec96ad8adc31a4c18d2307c7013f77
TomoakiTANAKA <tanaka1987@gmail.com>
2020-06-22 22:22:51 +0900

    insert iframe from slideshare

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,37 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
+import Iframe from 'react-iframe'
+
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title={siteTitle} />
       <Bio />
+
+      <h2>データでみる東京都知事選2020</h2>
+      <div style={
+        { width: '100%'
+       }
+      }>
+        <iframe src="//www.slideshare.net/slideshow/embed_code/key/C4MdOBFbOP8nRQ" 
+                width="100%" 
+                height="320px"
+                frameborder="0" 
+                marginwidth="0" 
+                marginheight="0" 
+                scrolling="no" 
+                style={
+                  {border: '1px solid #CCC', marginBottom: '5px', width: '100%'}
+                } 
+                allowfullscreen
+        />
+      </div>
+
+      <h2>候補者一覧</h2>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
